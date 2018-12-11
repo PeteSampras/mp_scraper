4866f17e5008c9f44c7c6c24e750d75e3a1b014b
root <root@localhost.localdomain>
2018-12-10 22:01:14 -1000

    fixed so it works in single file

--- cp_modules/option_menus.py
+++ cp_modules/option_menus.py
@@ -2,149 +2,138 @@ import configparser
 import os
 import sys
 from cp_modules.utility import colorize
-from cp_modules.multiproc import jobs,multProc,scrape,start_queue,done_queue,worker
-from multiprocessing import Process, Queue, freeze_support
-import time
+#from cp_modules.multiproc import jobs,multProc,scrape,start_queue,done_queue,worker
+#from multiprocessing import Process, Queue, freeze_support
+#import time
+#from new import process_queue,done_queue,add_to_queue
+
 
 config_file="domains.ini"
 config = configparser.ConfigParser()            # setup
 NUMBER_OF_PROCESSES = 4    # global cpu define
 
+def menu_options(selection):
+    if selection==None:
+        options = ['MAIN MENU','Add domain','Start processing queue','Stop processing queue','Display logs']
+        for i in range(1,5):
+            print(str(i) + '. '+options[i])
+    else:
+        options = ['SUB MENU']
+        print (colorize(options[0],'pink'))
+        print('1. %s' % selection)
+        print('b. Back')
+    print ('q. Quit\n')
+
 def mainmenu():
-    d = None
-    options = ['','Add domain','Start processing queue','Stop processing queue','Display logs']
-    msg = ''    
-    while d == None:
-        print (colorize('\nM A I N   M E N U','pink'))
-        print ('1. %s' % options[1])
-        print ('2. %s' % options[2])
-        print ('3. %s' % options[3])
-        print ('4. %s' % options[4])
-        print ('q. Quit')
-        option = input ('Select an option: ')
-        if option.lower () == 'q':
-            sys.exit ()
-        elif option == '1':
-            msg = options[1]    
-            d = add_domain_name()
-        elif option == '2':
-            msg =  options[2]    
-            d = start_processing()
-        elif option == '3':
-            msg =  options[3]    
-            d = stop_processing()
-        elif option == '4':
-            msg =  options[4]   
-            d = display_logs()
-        else:
-            print (colorize('Invalid selection!','warning'))
-    return msg, d
+    while True:
+        menu_options(None)
+        d = None
+        while d == None:
+            option = input ('Select an option: ')
+            if option.lower () == 'q':
+                os._exit(0)
+            elif option == '1':
+                d = add_domain_name()
+            elif option == '2':
+                d = start_processing()
+            elif option == '3':
+                d = stop_processing()
+            elif option == '4':
+                d = display_logs()
+            else:
+                print (colorize('Invalid selection!','warning'))
 
 def start_processing():
+    menu_options("Process known domains")
     p = None
     while p == None:
-        print (colorize('\nS U B  M E N U','pink'))
-        print ('1. Process known domains')
-        print ('b. Back')
-        print ('q. Quit')
         option = input ('Select an option: ')
         if option.lower () == 'q':
-            sys.exit ()
+            os._exit(0)
         elif option.lower () == 'b':
-            mainmenu ()
-            return
+            p = mainmenu()
         elif option == '1':
-            global start_queue
+            global process_queue
             global done_queue
             config.read(config_file)                        # read file
-            print('hi')
             for each_section in config.sections():
                 if each_section=='Domains':
                     for (each_key,each_val) in config.items(each_section):
-                        #freeze_support()
-                        #this=(scrape,each_key)
-                        print(each_key)
-                        start_queue.put(each_key)
-            process()
-            mainmenu()
-            return
+                        if "http" in each_key:
+                            add_to_queue(each_key)
+                        else:
+                            this = 'https://'
+                            if not 'www' in each_key:
+                                this = this + 'www.' + each_key
+                            else:
+                                this = this + each_key
+                            add_to_queue(this)
+                            print(this)
+            p=mainmenu()
         else:
             print (colorize('Invalid selection!','warning'))
-    mainmenu()
+            p=mainmenu()
+    p = mainmenu()
     return p
 
-def process():
-    print('process')
-    freeze_support()
-    for i in range(NUMBER_OF_PROCESSES):
-        Process(target=worker, args=(start_queue, done_queue))
 
-    for each_section in config.sections():
-        if each_section=='Domains':
-            for (each_key,each_val) in config.items(each_section):
-                start_queue.put(each_key)
-    
-    for message in iter(done_queue.get, 'STOP'):
-        print(message)
+# def stop_processing():
+#     p = None
+#     while p == None:
+#         print (colorize('\nS U B  M E N U','pink'))
+#         print ('1. Add domain')
+#         print ('b. Back')
+#         print ('q. Quit')
+#         option = input ('Select an option: ')
+#         if option.lower () == 'q':
+#             sys.exit ()
+#         elif option.lower () == 'b':
+#             mainmenu ()
+#             return
+#         elif option == '1':
+#             p = 'Do something'
+#         else:
+#             print ('Invalid selection!')
+#     return p
 
+# def display_logs():
+#     p = None
+#     while p == None:
+#         print (colorize('\nS U B  M E N U','pink'))
+#         print ('1. Add domain')
+#         print ('b. Back')
+#         print ('q. Quit')
+#         option = input ('Select an option: ')
+#         if option.lower () == 'q':
+#             sys.exit ()
+#         elif option.lower () == 'b':
+#             mainmenu ()
+#             return
+#         elif option == '1':
+#             p = 'Do something'
+#         else:
+#             print ('Invalid selection!')
+#     return p
 
-def stop_processing():
-    p = None
-    while p == None:
-        print (colorize('\nS U B  M E N U','pink'))
-        print ('1. Add domain')
-        print ('b. Back')
-        print ('q. Quit')
-        option = input ('Select an option: ')
-        if option.lower () == 'q':
-            sys.exit ()
-        elif option.lower () == 'b':
-            mainmenu ()
-            return
-        elif option == '1':
-            p = 'Do something'
-        else:
-            print ('Invalid selection!')
-    return p
-
-def display_logs():
-    p = None
-    while p == None:
-        print (colorize('\nS U B  M E N U','pink'))
-        print ('1. Add domain')
-        print ('b. Back')
-        print ('q. Quit')
-        option = input ('Select an option: ')
-        if option.lower () == 'q':
-            sys.exit ()
-        elif option.lower () == 'b':
-            mainmenu ()
-            return
-        elif option == '1':
-            p = 'Do something'
-        else:
-            print ('Invalid selection!')
-    return p
-
-def add_domain_name():
-    p=None
-    config.read(config_file)                        # read file
-    print(colorize('\r\nKnown domains:','pink'))
-    for each_section in config.sections():
-        if each_section=='Domains':
-            for (each_key,each_val) in config.items(each_section):
-                print(colorize(each_key, 'green'))
-    domain = input ('Domain to add: ')
-    for each_section in config.sections():
-        if each_section=='Domains':
-            for (each_key,each_val) in config.items(each_section):
-                if each_key==domain:
-                    p=True
-                    this = "%s is already added as a domain." % domain
-                    print(colorize(this,'fail'))
-                    mainmenu()
-    config.set('Domains',domain,'html')
-    with open(config_file, 'w') as configfile:
-        config.write(configfile)
-    print(colorize(domain + ' added.', 'green'))
-    return p
+# def add_domain_name():
+#     p=None
+#     config.read(config_file)                        # read file
+#     print(colorize('\r\nKnown domains:','pink'))
+#     for each_section in config.sections():
+#         if each_section=='Domains':
+#             for (each_key,each_val) in config.items(each_section):
+#                 print(colorize(each_key, 'green'))
+#     domain = input ('Domain to add: ')
+#     for each_section in config.sections():
+#         if each_section=='Domains':
+#             for (each_key,each_val) in config.items(each_section):
+#                 if each_key==domain:
+#                     p=True
+#                     this = "%s is already added as a domain." % domain
+#                     print(colorize(this,'fail'))
+#                     mainmenu()
+#     config.set('Domains',domain,'html')
+#     with open(config_file, 'w') as configfile:
+#         config.write(configfile)
+#     print(colorize(domain + ' added.', 'green'))
+#     return p
--- new.py
+++ new.py
@@ -3,8 +3,160 @@ import argparse
 from cp_modules.configure import configuration
 import configparser
 import sys
-from cp_modules.option_menus import  mainmenu
 from cp_modules.utility import colorize,header
+import requests
+import multiprocessing
+from multiprocessing import Process, Queue, current_process, freeze_support
+import os
+#from queue import Queue
+#from cp_modules.option_menus import menu_options,mainmenu,start_processing#,stop_processing
+
+config_file="domains.ini"
+config = configparser.ConfigParser()            # setup
+NUM_WORKERS = 4
+
+done_queue = Queue() # This is messages from the child processes for parent
+process_queue = Queue() # This is the domains to process
+
+def scraper(input, output):
+  # output.put("{} starting".format(current_process().name))
+  for domain in iter(input.get, 'STOP'):
+    result = requests.get(domain)
+    output.put("{}: Domain {} retrieved with {} bytes".format(current_process().name, domain, len(result.text)))
+
+def add_to_queue(domain):
+    process_queue.put(domain)
+
+def process_done_queue(input):
+    for message in iter(input.get, 'STOP'):
+        print(message)
+
+def main():
+    for i in range(NUM_WORKERS):
+        Process(target=scraper, args=(process_queue, done_queue)).start()
+    Process(target=process_done_queue, args=(done_queue,)).start()
+    mainmenu()
+
+def menu_options(selection):
+    if selection==None:
+        options = ['MAIN MENU','Add domain','Start processing queue','Stop processing queue','Display logs']
+        for i in range(1,5):
+            print(str(i) + '. '+options[i])
+    else:
+        options = ['SUB MENU']
+        print (colorize(options[0],'pink'))
+        print('1. %s' % selection)
+        print('b. Back')
+    print ('q. Quit\n')
+
+def mainmenu():
+    while True:
+        menu_options(None)
+        d = None
+        while d == None:
+            option = input ('Select an option: ')
+            if option.lower () == 'q':
+                os._exit(0)
+            elif option == '1':
+                d = add_domain_name()
+            elif option == '2':
+                d = start_processing()
+            elif option == '3':
+                d = stop_processing()
+            elif option == '4':
+                d = display_logs()
+            else:
+                print (colorize('Invalid selection!','warning'))
+
+def start_processing():
+    menu_options("Process known domains")
+    p = None
+    while p == None:
+        option = input ('Select an option: ')
+        if option.lower () == 'q':
+            os._exit(0)
+        elif option.lower () == 'b':
+            p = mainmenu()
+        elif option == '1':
+            global process_queue
+            global done_queue
+            config.read(config_file)                        # read file
+            for each_section in config.sections():
+                if each_section=='Domains':
+                    for (each_key,each_val) in config.items(each_section):
+                        if "http" in each_key:
+                            add_to_queue(each_key)
+                        else:
+                            this = 'https://'
+                            if not 'www' in each_key:
+                                this = this + 'www.' + each_key
+                            else:
+                                this = this + each_key
+                            add_to_queue(this)
+                            print(this)
+            p=mainmenu()
+        else:
+            print (colorize('Invalid selection!','warning'))
+            p=mainmenu()
+    p = mainmenu()
+    return p
+
+def stop_processing():
+    menu_options('Confirm cancellation')
+    p = None
+    while p == None:
+        option = input ('Select an option: ')
+        if option.lower() == 'q':
+            os._exit(0)
+        elif option.lower() == 'b':
+            p = mainmenu()
+        elif option == '1':
+            print('Stopping all processes')
+            process_queue.put('STOP')
+            done_queue.put('STOP')
+            p = mainmenu()
+        else:
+            print ('Invalid selection!')
+    return p
+
+def add_domain_name():
+    p=None
+    config.read(config_file)                        # read file
+    print(colorize('\r\nKnown domains:','pink'))
+    for each_section in config.sections():
+        if each_section=='Domains':
+            for (each_key,each_val) in config.items(each_section):
+                print(colorize(each_key, 'green'))
+    domain = input ('Domain to add: ')
+    for each_section in config.sections():
+        if each_section=='Domains':
+            for (each_key,each_val) in config.items(each_section):
+                if each_key==domain:
+                    p=True
+                    this = "%s is already added as a domain." % domain
+                    print(colorize(this,'fail'))
+                    mainmenu()
+    config.set('Domains',domain,'html')
+    with open(config_file, 'w') as configfile:
+        config.write(configfile)
+    print(colorize(domain + ' added.', 'green'))
+    return p
+
+def display_logs():
+    #for each in iter(done_queue.get,'STOP'):
+    #    print(str(each))
+    #    pass
+    return p
+
+
+
+
+
+
+
+
+
+
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(prog='Multiprocess',
@@ -17,8 +169,12 @@ if __name__=='__main__':
     print(colorize('version 1.0\n', 'green'))
 
     try:
-        mainmenu()
+        main()
     except KeyboardInterrupt:
         print("Keyboard interrupted")
+    except Exception as e:
+        print(e)
     finally:
-        sys.exit()   
+        print('Finished')
+        os._exit(0)
+
