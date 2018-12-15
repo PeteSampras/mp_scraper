2315db51a26289a4ee9f4e7e9c11aafb0622e147
root <root@localhost.localdomain>
2018-12-15 13:57:15 -1000

    changed code up

--- cp_modules/test2.py
+++ cp_modules/test2.py
@@ -10,129 +10,128 @@ process_queue = Queue() # This is the domains to process
 website = 'https://www.devleague.com'
 
 manager = Manager()
-shared_list = manager.list()
+start_list = manager.list()
 master_list = manager.list()
+ignore_list=manager.list()
 done_list = manager.list()
 mail_list=manager.list()
 http_list=manager.list()
 diff_list = manager.list()
-shared_list.append(website)
-i=0
+master_list.append(website)
+start_list.append(website)
 
-def get_listing(url):
-    global done_list
-    global shared_list
-    global mail_list
-    global http_list
-    if url in done_list:
-        return None
-    print(done_list)
-    print('shared: {} , done {}, mail {}, http {}: ' .format(len(shared_list), len(done_list), len(mail_list), len(http_list)))
-    # print(url)
+
+
+def get_listing(url,masterlist,donelist):
+    print('url: {} , master {}, done {}, ' .format(url, len(masterlist), len(donelist)))
     # headers = {
     #     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
     html = None
     links = None
+    donelist.append(url)
     if url.startswith('/'):
         url = website+url
     r = requests.get(url, timeout=10)         
-
     if r.status_code == 200:
         html = r.text
         soup = BeautifulSoup(html, 'html.parser')
         listing_section = soup.findAll('a', href=True)
         links = [link['href'].strip() for link in listing_section]
-
         #global done_list
         try:
             for link in links:
-                if not link in done_list and not link in shared_list:
-                    this = parse(link)
-                    if this != None and this != website:
-                        new_url=url+link
-                        shared_list.append(new_url)
-            shared_list=list(set(shared_list))
-            done_list=list(set(done_list))
+                masterlist.append(link)
         except Exception as e:
             print(e)
-            return None
-    return links
 
-def parse(links):
-    global done_list
-    global shared_list
-    global mail_list
-    global http_list
-    #links=list(set(links)) # get uniques
-    if links.startswith('//') or links == website:
-        http_list.append(links.replace('//',''))
+
+def parse(link,mail,http):
+    if link.startswith('//'):
+        link = link[2:]
+        if not link in http:
+            http.append(link)
         return None
-    if not links.startswith('//'):
-        if links != '/' and not links.startswith('#') and '/' in links:
-            if website in links:
-                link = links.replace(website,'')
+    if not link.startswith('//'):
+        if link != '/' and not link.startswith('#') and '/' in link:
+            if website in link:
+                link = link.replace(website,'')
                 return link
-            if 'http' in links:
-                http_list.append(links)
+            if 'http' in link.lower() or 'www' in link.lower():
+                http_list.append(link)
                 return None
-            elif 'mailto:' in links:
-                mail_list.append(links)
+            elif 'mailto:' in link.lower():
+                mail_list.append(link)
                 return None
             else:
-                return links
+                return link
     return None
 
-def do_stuff(url_list):
-    #print(url_list)
-    global done_list
-    if url_list==website and website in done_list:
-        return
-    global shared_list
-    this = diff(shared_list,done_list)
+def do_stuff(args):
+    print(os.getpid())
+    # difflist,startlist,donelist,httplist,maillist,masterlist
+    difflist=args[0]
+    startlist=args[1]
+    donelist=args[2]
+    httplist=args[3]
+    maillist=args[4]
+    masterlist=args[5]
+    ignorelist=args[6]
+    this = diff(startlist,donelist)
     for url in this:
-        get_urls = get_listing(url)
+        get_urls = get_listing(url,masterlist,donelist)
         if get_urls!=None:
             for each_url in get_urls:
-                if each_url!=None:
-                    if not url in done_list:
-                        done_list.append(url)
-                        shared_list.append(each_url)
-
-
-    done_list=list(set(done_list))
-    shared_list=list(set(shared_list))
-    #print(len(diff(shared_list,done_list)))
+                masterlist.append(each_url)
+    update_master(masterlist,ignorelist,startlist,donelist,maillist,httplist)
+    print('Done:'+str(donelist)+ ' start: '+ str(startlist))
+    donelist=list(set(donelist))
+    startlist=list(set(startlist))
+    difflist=diff(startlist,donelist)
+    difflist=list(set(difflist))
 
 def diff(first, second):
-        second = set(second)
-        return [item for item in first if item not in second]
+    second = set(second)
+    return [item for item in first if item not in second]
+
+def update_master(master,ignore,start,done,mail,http):
+    this = diff(master,ignore)
+    this = diff(this,done)
+    this = diff(this,start)
+    #print('Master:'+str(this))
+    for url in this:
+        if parse(url,mail,http) != None:
+            start.append(url)
+        else:
+            ignore.append(url)
+    #print("Start: "+str(start))
+    #print("Ignore: " + str(ignore))
+
+
 
 
 def main():
-    global shared_list
+    global start_list
     global done_list
     global diff_list
     global master_list
     global mail_list
     global http_list
-    global i
+    global ignore_list
     pool = Pool(processes=10)
-    while len(diff(shared_list,master_list))>0:
-        if len(shared_list) < 1 and i > 3:
-            done()
-            os._exit(0)
-        i = i + 1
-        if len(mail_list)>0:
-            http_list=list(set(http_list))
-        if len(mail_list)>0:
-            mail_list=list(set(mail_list))
-        shared_list=list(set(shared_list))
-        diff_list=diff(shared_list,done_list)
+    while len(diff(start_list,done_list))>0:
+        #print('Main: ' + str(start_list))
+        master_list=list(set(master_list))
+        ignore_list=list(set(ignore_list))
+        start_list=list(set(start_list))
+        diff_list=diff(start_list,done_list)
         diff_list=list(set(diff_list))
-        pool.map(do_stuff, diff_list)
+        print('Diff: '+str(diff_list))
+        pool.map(do_stuff, [(diff_list,start_list,done_list,http_list,mail_list,master_list,ignore_list)])
+    pool.close()
+    done()
 
 def done():
-    global shared_list
+    global start_list
     global done_list
     global diff_list
     global master_list
@@ -141,9 +140,9 @@ def done():
     print(done_list)
     print(mail_list)
     print(http_list)
-    pool.close()
-    #print(list(set(shared_list)))
-    print('Finished')
+
+    #print(list(set(start_list)))
+    print('Big Success')
     os._exit(0)
 
 if __name__=='__main__':
